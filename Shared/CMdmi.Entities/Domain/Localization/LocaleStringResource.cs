d25f74a98941f1fe4bdacdde9c7a8c987bb00960
Laolu Olapegba <laoluolapegba@gmail.com>
2018-02-15 12:16:51 +0100

    Permissionsservie , basepaging for large items

---
+++ Shared/CMdmi.Entities/Domain/Localization/LocaleStringResource.cs
@@ -0,0 +1,35 @@
+﻿using CMdm.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CMdm.Entities.Domain.Localization
+{
+    /// <summary>
+    /// Represents a locale string resource
+    /// </summary>
+    public partial class LocaleStringResource : BaseEntity
+    {
+        /// <summary>
+        /// Gets or sets the language identifier
+        /// </summary>
+        public int LanguageId { get; set; }
+
+        /// <summary>
+        /// Gets or sets the resource name
+        /// </summary>
+        public string ResourceName { get; set; }
+
+        /// <summary>
+        /// Gets or sets the resource value
+        /// </summary>
+        public string ResourceValue { get; set; }
+
+        /// <summary>
+        /// Gets or sets the language
+        /// </summary>
+        public virtual Language Language { get; set; }
+    }
+}
