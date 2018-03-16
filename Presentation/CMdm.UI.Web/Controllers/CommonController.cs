2e8c49b9cca8e0e54230774da74a2da1cacadd36
Laolu Olapegba <laoluolapegba@gmail.com>
2018-03-16 11:51:19 +0100

    SystemInfo

---
+++ Presentation/CMdm.UI.Web/Controllers/CommonController.cs
@@ -0,0 +1,140 @@
+﻿using CMdm.Core;
+using CMdm.UI.Web.Models.Common;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.InteropServices;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CMdm.UI.Web.Controllers
+{
+    public class CommonController : BaseController
+    {
+        
+
+        private readonly HttpContextBase _httpContext;
+
+        public CommonController()
+        {
+            //HttpContextBase httpContext,
+            HttpContextBase abstractContext = new System.Web.HttpContextWrapper(System.Web.HttpContext.Current);
+            this._httpContext = abstractContext;
+        }
+        // GET: System
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public virtual ActionResult SystemInfo()
+        {
+            if (!User.Identity.IsAuthenticated)
+                return AccessDeniedView();
+
+            var model = new SystemInfoModel();
+            model.Version = CmdmVersion.CurrentVersion;
+            try
+            {
+                model.OperatingSystem = Environment.OSVersion.VersionString;
+            }
+            catch (Exception) { }
+            try
+            {
+                model.AspNetInfo = RuntimeEnvironment.GetSystemVersion();
+            }
+            catch (Exception) { }
+            try
+            {
+                model.IsFullTrust = AppDomain.CurrentDomain.IsFullyTrusted.ToString();
+            }
+            catch (Exception) { }
+            model.ServerTimeZone = TimeZone.CurrentTimeZone.StandardName;
+            model.ServerLocalTime = DateTime.Now;
+            model.UtcTime = DateTime.UtcNow;
+            model.CurrentUserTime = DateTime.Now;//_dateTimeHelper.ConvertToUserTime(DateTime.Now);
+            model.HttpHost = Request.ServerVariables["HTTP_HOST"];
+            foreach (var key in _httpContext.Request.ServerVariables.AllKeys)
+            {
+                if (key.StartsWith("ALL_")) continue;
+
+                model.ServerVariables.Add(new SystemInfoModel.ServerVariableModel
+                {
+                    Name = key,
+                    Value = _httpContext.Request.ServerVariables[key]
+                });
+            }
+            //Environment.GetEnvironmentVariable("USERNAME");
+
+            var trustLevel = CommonHelper.GetTrustLevel();
+
+            foreach (var assembly in AppDomain.CurrentDomain.GetAssemblies())
+            {
+                var loadedAssembly = new SystemInfoModel.LoadedAssembly
+                {
+                    FullName = assembly.FullName,
+
+                };
+                //ensure no exception is thrown
+                try
+                {
+                    var canGetLocation = trustLevel >= AspNetHostingPermissionLevel.High && !assembly.IsDynamic;
+                    loadedAssembly.Location = canGetLocation ? assembly.Location : null;
+                    loadedAssembly.IsDebug = IsDebugAssembly(assembly);
+                    loadedAssembly.BuildDate = canGetLocation ? (DateTime?)GetBuildDate(assembly, TimeZoneInfo.Local) : null;
+                }
+                catch (Exception) { }
+                model.LoadedAssemblies.Add(loadedAssembly);
+            }
+
+            return View(model);
+        }
+        #region Utitlies
+
+        private bool IsDebugAssembly(Assembly assembly)
+        {
+            var attribs = assembly.GetCustomAttributes(typeof(System.Diagnostics.DebuggableAttribute), false);
+
+            if (attribs.Length > 0)
+            {
+                var attr = attribs[0] as System.Diagnostics.DebuggableAttribute;
+                if (attr != null)
+                {
+                    return attr.IsJITOptimizerDisabled;
+                }
+            }
+
+            return false;
+        }
+
+        private DateTime GetBuildDate(Assembly assembly, TimeZoneInfo target = null)
+        {
+            var filePath = assembly.Location;
+
+            const int cPeHeaderOffset = 60;
+            const int cLinkerTimestampOffset = 8;
+
+            var buffer = new byte[2048];
+
+            using (var stream = new FileStream(filePath, FileMode.Open, FileAccess.Read))
+            {
+                stream.Read(buffer, 0, 2048);
+            }
+
+            var offset = BitConverter.ToInt32(buffer, cPeHeaderOffset);
+            var secondsSince1970 = BitConverter.ToInt32(buffer, offset + cLinkerTimestampOffset);
+            var epoch = new DateTime(1970, 1, 1, 0, 0, 0, DateTimeKind.Utc);
+
+            var linkTimeUtc = epoch.AddSeconds(secondsSince1970);
+
+            var tz = target ?? TimeZoneInfo.Local;
+            var localTime = TimeZoneInfo.ConvertTimeFromUtc(linkTimeUtc, tz);
+
+            return localTime;
+        }
+
+        #endregion
+    }
+}
