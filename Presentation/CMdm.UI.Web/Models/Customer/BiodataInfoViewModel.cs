33511a83fd033e5970e1999b69cc06a8a3b508dc
Abass Gbadeyanka <agbadeyanka@bluechiptech.biz>
2018-04-11 16:53:48 +0100

    Biodata Edit completed

---
+++ Presentation/CMdm.UI.Web/Models/Customer/BiodataInfoViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CMdm.UI.Web.Models.Customer
+{
+    public class BiodataInfoViewModel
+    {
+        public IndividualBioDataModel BioData { get; set; }
+        public IndividualAddressDetails AddressDetails { get; set; }
+        public individualIDDetail identification { get; set; }
+        public OtherDetails otherdetails { get; set; }
+        public IndividualContactDetails contact { get; set; }
+    }
+}
---
+++ Presentation/CMdm.UI.Web/Models/Customer/OtherDetails.cs
@@ -0,0 +1,22 @@
+﻿namespace CMdm.UI.Web.Models.Customer
+{
+    using System;
+    using System.Collections.Generic;
+    using System.ComponentModel;
+    using System.Linq;
+    using System.Web;
+    public class OtherDetails
+    {
+       public string CUSTOMER_NO { get; set; }
+        [DisplayName("TIN Number")]
+        public string TIN_NO { get; set; }
+        public Nullable<System.DateTime> CREATED_DATE { get; set; }
+        public string CREATED_BY { get; set; }
+        public Nullable<System.DateTime> LAST_MODIFIED_DATE { get; set; }
+
+        public string AUTHORISED { get; set; }
+        public string AUTHORISED_BY { get; set; }
+        public Nullable<System.DateTime> AUTHORISED_DATE { get; set; }
+        public string IP_ADDRESS { get; set; }
+    }
+}
